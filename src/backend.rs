fbd72f864ca2455f6907f2ea727b4e2cffcd9bbb
Cheuk Yin Ng <chucksys88@gmail.com>
2024-09-08 13:33:16 -0700

    wip: basic capabilities, workspace folders
    skelly for potential namespace reads

--- src/backend.rs
+++ src/backend.rs
@@ -49,8 +49,47 @@ impl Backend {
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+    async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
+        let mut workspace_folders = params.workspace_folders.unwrap_or(vec![]);
+        if workspace_folders.len() == 0 {
+            if let Some(root_uri) = params.root_uri {
+                workspace_folders.push(WorkspaceFolder {
+                    uri: root_uri.clone(),
+                    name: root_uri.to_string()
+                });
+            } else {
+                // deprecated options, but we should check for them nonetheless
+                if let Some(root_path) = params.root_path {
+                    if let Ok(root_uri) = Url::from_directory_path(&root_path) {
+                        workspace_folders.push(WorkspaceFolder {
+                            uri: root_uri,
+                            name: root_path
+                        });
+                    }
+                }
+            }
+        }
+
+        if workspace_folders.len() == 0 {
+            self.client
+                .log_message(MessageType::LOG, "unable to find workspace folders, root paths, or root uris")
+                .await;
+        } else {
+            self.client
+                .log_message(MessageType::LOG, format!("found {} workspace folders: {:?}", workspace_folders.len(), &workspace_folders))
+                .await;
+        }
+
+        // TODO check workspace folders for `composer.json` and read namespaces with PSR-4 and
+        // PSR-0 (maybe support it??)
+
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities::default(),
+            server_info: Some(ServerInfo {
+                name: env!("CARGO_PKG_NAME").to_string(),
+                version: Some(env!("CARGO_PKG_VERSION").to_string()),
+            }),
+        })
     }
 
     async fn initialized(&self, _: InitializedParams) {
