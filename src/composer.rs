26aa33a1d668b19f343c8270552b24ad365d4d64
Cheuk Yin Ng <chucksys88@gmail.com>
2025-03-03 23:16:47 -0800

    refactor: partial deserde of composer
    don't manually parse it, but use `struct`s.

--- src/composer.rs
+++ src/composer.rs
@@ -1,4 +1,5 @@
 use serde_json::{Error as SerdeError, Map as SerdeMap, Value as SerdeValue};
+use serde::Deserialize;
 
 use std::collections::HashMap;
 use std::error::Error;
@@ -8,12 +9,35 @@ use std::str::FromStr;
 
 use crate::php_namespace::PhpNamespace;
 
+#[derive(Deserialize)]
+struct ComposerScheme {
+    autoload: Option<AutoloadScheme>,
+}
+
+#[derive(Deserialize)]
+struct AutoloadScheme {
+    #[serde(rename(deserialize = "psr-4"))]
+    psr4: Option<NamespacePathScheme>,
+    #[serde(rename(deserialize = "psr-0"))]
+    psr0: Option<NamespacePathScheme>,
+    files: Option<Vec<String>>,
+}
+
+#[derive(Deserialize)]
+struct NamespacePathScheme(HashMap<String, PathScheme>);
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum PathScheme {
+    SinglePath(String),
+    MultiplePaths(Vec<String>),
+}
+
 #[derive(Debug)]
 pub enum AutoloadError {
     BadDeserde(SerdeError),
     NoAutoload,
     NoPSR4,
-    BadPSR4Type,
 }
 
 impl PartialEq for AutoloadError {
@@ -34,7 +58,6 @@ impl Display for AutoloadError {
             AutoloadError::BadDeserde(e) => write!(f, "serde error: {}", e),
             AutoloadError::NoAutoload => write!(f, "no autoload given"),
             AutoloadError::NoPSR4 => write!(f, "no psr-4 in autoload"),
-            AutoloadError::BadPSR4Type => write!(f, "malformed psr-4 value type"),
         }
     }
 }
@@ -49,45 +72,25 @@ pub struct Autoload {
 }
 
 impl Autoload {
-    fn from_psr4_map(h: &SerdeMap<String, SerdeValue>) -> Self {
-        let mut psr4 = HashMap::new();
-        for (ns, dir) in h.into_iter() {
-            let namespace = PhpNamespace::from_str(&ns).unwrap();
-            match dir {
-                SerdeValue::String(s) => {
-                    psr4.insert(namespace, vec![PathBuf::from_str(&s).unwrap()]);
-                }
-                SerdeValue::Array(dirs) => {
-                    let mut paths = Vec::with_capacity(dirs.len());
-
-                    for dir in dirs {
-                        if let SerdeValue::String(s) = dir {
-                            paths.push(PathBuf::from_str(&s).unwrap());
-                        }
-                    }
-
-                    psr4.insert(namespace, paths);
-                }
-                _ => continue,
-            }
-        }
-
-        Self { psr4 }
-    }
-
     pub fn from_reader<R>(rdr: R) -> Result<Self, AutoloadError>
     where
         R: std::io::Read,
     {
-        let v: serde_json::Value = serde_json::from_reader(rdr)?;
-        let autoload = v.get("autoload").ok_or(AutoloadError::NoAutoload)?;
+        let mut psr4_ret = HashMap::new();
 
-        // TODO support different types of autoloading
-        let psr4_value = autoload.get("psr-4").ok_or(AutoloadError::NoPSR4)?;
-        match psr4_value {
-            SerdeValue::Object(hash) => Ok(Self::from_psr4_map(hash)),
-            _ => Err(AutoloadError::BadPSR4Type),
+        let composer: ComposerScheme = serde_json::from_reader(rdr)?;
+        let autoload = composer.autoload.ok_or(AutoloadError::NoAutoload)?;
+        let psr4 = autoload.psr4.ok_or(AutoloadError::NoPSR4)?;
+        for (ns_str, paths) in &psr4.0 {
+            let ns = PhpNamespace::from_str(ns_str).unwrap();
+            let paths = match paths {
+                PathScheme::SinglePath(p) => vec![PathBuf::from_str(&p).unwrap()],
+                PathScheme::MultiplePaths(vec) => vec.iter().map(|p| PathBuf::from_str(&p).unwrap()).collect(),
+            };
+            psr4_ret.insert(ns, paths);
         }
+
+        Ok(Self { psr4: psr4_ret })
     }
 }
 
@@ -150,30 +153,29 @@ mod test {
             },
         }));
 
-        assert_eq!(Autoload::from_reader(data), Err(AutoloadError::BadPSR4Type));
-    }
-
-    #[test]
-    fn nothing_in_it() {
-        let j = json!({});
-        let m = to_map(j);
-
-        assert_eq!(Autoload::from_psr4_map(&m).psr4.len(), 0);
+        match Autoload::from_reader(data) {
+            Err(AutoloadError::BadDeserde(_)) => {},
+            x => panic!("{:?}", x),
+        }
     }
 
     #[test]
     fn kv() {
-        let j = json!({
-            "Monolog\\": "src/",
-            "Vendor\\Namespace\\": [
-                "vendor/",
-                "namespace/",
-                12,
-                13,
-            ],
-        });
-        let m = to_map(j);
-        let a = Autoload::from_psr4_map(&m);
+        let data = to_cursor(json!({
+            "autoload": {
+                "psr-4": {
+                    "Monolog\\": "src/",
+                    "Vendor\\Namespace\\": [
+                        "vendor/",
+                        "namespace/",
+                    ],
+                },
+            },
+        }));
+        let a = match Autoload::from_reader(data) {
+            Ok(x) => x,
+            Err(e) => panic!("{:?}", e),
+        };
 
         assert_eq!(a.psr4.len(), 2);
 
